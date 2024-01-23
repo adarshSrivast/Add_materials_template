5b4f0f23fb91a1c80f708b621a75c4248dcc3741
adarshSrivast <149382328+adarshSrivast@users.noreply.github.com>
2024-01-23 09:49:35 +0530

    Add files via upload

---
+++ layouts/start-learning-layout.vue
@@ -0,0 +1,67 @@
+<template>
+    <main class="container-fluid px-0">
+        <div class="start-learning-header">
+            <a target="_blank">
+                <i class="fa fa-question-circle"></i>
+            </a>
+            <h1>Start Learning</h1>
+            <a href="" @click="handleClick()">
+                <i class="fa fa-times"></i>
+            </a>
+        </div>
+    </main>
+
+    <slot/>
+</template>
+
+<style scoped>
+.container-fluid {
+    background-color: #8723ff;
+    color: white;
+}
+.start-learning-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 56px;
+    padding: 0px 16px;
+}
+
+h1{
+    font-size: 12px;
+    line-height: 14px;
+}
+
+i{
+    color: white;
+}
+@media(min-width : 768px){
+    .start-learning-header{
+        padding: 0px 32px;
+    }
+
+}
+
+@media(min-width : 992px){
+    .start-learning-header{
+        padding: 0px 24px;
+    }
+}
+
+@media(min-width : 1280px){
+    .container-fluid{
+        display: flex;
+        justify-content: center;
+    }
+    .start-learning-header{
+        min-width : 1280px;
+    }
+
+}
+
+</style>
+
+<script setup>
+
+
+</script>
